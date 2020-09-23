8c5f2c17f0f350c00e19b767424b8dc8b7d2d28f
monfrair <williamsorrentino@gmail.com>
2020-09-23 11:11:20 -0400

    adding js functionality

---
+++ script.js
@@ -0,0 +1,15 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt user to select media stream, pass to video element, then play
+
+async function selectMediaStream() {
+	try {
+		something;
+	} catch (error) {
+		// catch any errors here
+		console.log('whoops, error here:', error);
+	}
+}
+// On load
+selectMediaStream();
